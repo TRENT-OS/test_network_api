56cae93738228f61d9fc45f2f04736eacfdc0157
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-04-03 10:26:58 +0200

    [FOR] Astyle

--- seos_system_config.h
+++ seos_system_config.h
@@ -6,30 +6,27 @@
  */
 #pragma once
 
-
 //-----------------------------------------------------------------------------
 // Debug
 //-----------------------------------------------------------------------------
 
 #if !defined(NDEBUG)
-#   define Debug_Config_STANDARD_ASSERT
-#   define Debug_Config_ASSERT_SELF_PTR
+#define Debug_Config_STANDARD_ASSERT
+#define Debug_Config_ASSERT_SELF_PTR
 #else
-#   define Debug_Config_DISABLE_ASSERT
-#   define Debug_Config_NO_ASSERT_SELF_PTR
+#define Debug_Config_DISABLE_ASSERT
+#define Debug_Config_NO_ASSERT_SELF_PTR
 #endif
 
-#define Debug_Config_LOG_LEVEL              Debug_LOG_LEVEL_DEBUG
+#define Debug_Config_LOG_LEVEL Debug_LOG_LEVEL_DEBUG
 #define Debug_Config_INCLUDE_LEVEL_IN_MSG
 #define Debug_Config_LOG_WITH_FILE_LINE
-
 
 //-----------------------------------------------------------------------------
 // Memory
 //-----------------------------------------------------------------------------
 
 #define Memory_Config_USE_STDLIB_ALLOC
-
 
 //-----------------------------------------------------------------------------
 // ChanMUX
@@ -47,29 +44,26 @@ enum
     CHANMUX_CHANNEL_NIC_2_CTRL, // 7
     CHANMUX_CHANNEL_NIC_2_DATA, // 8
 
-    CHANMUX_NUM_CHANNELS        // 9
+    CHANMUX_NUM_CHANNELS // 9
 };
-
 
 //-----------------------------------------------------------------------------
 // Network Stack #1
 //-----------------------------------------------------------------------------
 
-#define ETH_1_ADDR                  "10.0.0.10"
-#define ETH_1_GATEWAY_ADDR          "10.0.0.1"
-#define ETH_1_SUBNET_MASK           "255.255.255.0"
-
+#define ETH_1_ADDR "10.0.0.10"
+#define ETH_1_GATEWAY_ADDR "10.0.0.1"
+#define ETH_1_SUBNET_MASK "255.255.255.0"
 
 //-----------------------------------------------------------------------------
 // Network Stack #2
 //-----------------------------------------------------------------------------
 
-#define ETH_2_ADDR                  "10.0.0.11"
-#define ETH_2_GATEWAY_ADDR          "10.0.0.1"
-#define ETH_2_SUBNET_MASK           "255.255.255.0"
-
+#define ETH_2_ADDR "10.0.0.11"
+#define ETH_2_GATEWAY_ADDR "10.0.0.1"
+#define ETH_2_SUBNET_MASK "255.255.255.0"
 
 //-----------------------------------------------------------------------------
 // Client Test App
 //-----------------------------------------------------------------------------
-#define CFG_TEST_HTTP_SERVER        "192.168.82.12"
+#define CFG_TEST_HTTP_SERVER "192.168.82.12"
