517f0d0671049d1ade393febab62491bff1845d3
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2020-04-15 20:30:26 +0200

    [DEV] SEOS-939: remove not needed Mutex in ChanMux

--- components/ChanMux/ChanMux.c
+++ components/ChanMux/ChanMux.c
@@ -152,7 +152,6 @@ ChanMux_getInstanceOrCreate(
         if (!ChanMux_ctor(&theOne,
                           channels,
                           &cfgChanMux,
-                          NULL,
                           ChanMux_dataAvailable_emit,
                           Output_write))
         {
