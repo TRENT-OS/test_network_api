08a122f366f3323081d3a3263338b6cc4a94ed6c
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-05-18 11:10:32 +0200

    [FOR] Applied formatting

--- components/TestAppClient/test_app_client.c
+++ components/TestAppClient/test_app_client.c
@@ -5,38 +5,37 @@
  *
  */
 
-#include <string.h>
-#include "stdint.h"
 #include "LibDebug/Debug.h"
 #include "SeosError.h"
+#include "stdint.h"
+#include <string.h>
 
 #include "OS_Network.h"
 
-extern seos_err_t OS_NetworkAPP_RT(OS_Network_Context_t ctx);
+extern seos_err_t
+OS_NetworkAPP_RT(OS_Network_Context_t ctx);
 
 /*
-    This example demonstrates reading of a web page example.com using Nw Stack API.
-    Currently only a single socket is supported per stack instance.
-    i.e. no multitasking is supported as of now.
+    This example demonstrates reading of a web page example.com using Nw Stack
+   API. Currently only a single socket is supported per stack instance. i.e. no
+   multitasking is supported as of now.
 
 */
 
 #define HTTP_PORT 80
 
-int run()
+int
+run()
 {
     Debug_LOG_INFO("Starting test_app_client...");
 
     char buffer[4096];
-    OS_NetworkAPP_RT(NULL);   // Must be actullay called by SEOS Runtime
+    OS_NetworkAPP_RT(NULL); // Must be actullay called by SEOS Runtime
 
-    OS_Network_Socket_t cli_socket =
-    {
-        .domain = OS_AF_INET,
-        .type   = OS_SOCK_STREAM,
-        .name   = CFG_TEST_HTTP_SERVER,
-        .port   = HTTP_PORT
-    };
+    OS_Network_Socket_t cli_socket = { .domain = OS_AF_INET,
+                                       .type   = OS_SOCK_STREAM,
+                                       .name   = CFG_TEST_HTTP_SERVER,
+                                       .port   = HTTP_PORT };
 
     /* This creates a socket API and gives an handle which can be used
        for further communication. */
@@ -55,15 +54,14 @@ int run()
         "GET / HTTP/1.0\r\nHost: 192.168.82.12\r\nConnection: close\r\n\r\n";
 
     const size_t len_request = strlen(request);
-    size_t len = len_request;
+    size_t       len         = len_request;
 
     /* Send the request to the host */
     size_t offs = 0;
     do
     {
         const size_t lenRemaining = len_request - offs;
-        size_t len_io = lenRemaining;
-
+        size_t       len_io       = lenRemaining;
 
         err = OS_NetworkSocket_write(handle, &request[offs], &len_io);
 
@@ -75,11 +73,10 @@ int run()
         }
 
         /* fatal error, this must not happen. API broken*/
-        Debug_ASSERT( len_io <= lenRemaining );
+        Debug_ASSERT(len_io <= lenRemaining);
 
         offs += len_io;
-    }
-    while (offs < len_request);
+    } while (offs < len_request);
 
     Debug_LOG_INFO("read response...");
 
@@ -89,9 +86,8 @@ int run()
      a. err = SEOS_ERROR_CONNECTION_CLOSED indicating end of data read
               and connection close
      b. err = SEOS_ERROR_GENERIC  due to error in read
-     c. err = SEOS_SUCCESS and length = 0 indicating no data to read but there is still
-              connection
-     d. err = SEOS_SUCCESS and length >0 , valid data
+     c. err = SEOS_SUCCESS and length = 0 indicating no data to read but there
+    is still connection d. err = SEOS_SUCCESS and length >0 , valid data
 
     Take appropriate actions based on the return value rxd.
 
@@ -107,7 +103,8 @@ int run()
     {
         len = sizeof(buffer);
 
-        /* Keep calling read until we receive CONNECTION_CLOSED from the stack */
+        /* Keep calling read until we receive CONNECTION_CLOSED from the stack
+         */
         memset(buffer, 0, sizeof(buffer));
 
         seos_err_t err = OS_NetworkSocket_read(handle, buffer, &len);
@@ -115,25 +112,25 @@ int run()
         switch (err)
         {
 
-        /* This means end of read or nothing further to read as socket was closed */
+        /* This means end of read or nothing further to read as socket was
+         * closed */
         case SEOS_ERROR_CONNECTION_CLOSED:
             Debug_LOG_INFO("socket_read() reported connection closed");
-            flag = false;    /* terminate loop and close handle*/
+            flag = false; /* terminate loop and close handle*/
             break;
 
         /* Success . continue further reading */
         case SEOS_SUCCESS:
             Debug_LOG_INFO("chunk read, length %d", len);
-            continue ;
+            continue;
 
         /* Error case, break and close the handle */
         default:
             Debug_LOG_INFO("socket_read() failed, error %d", err);
-            flag = false;    /* terminate loop and close handle */
+            flag = false; /* terminate loop and close handle */
             break;
-        }// end of switch
-    }
-    while (flag);
+        } // end of switch
+    } while (flag);
     Debug_LOG_INFO("Test ended");
     /* Close the socket communication */
     err = OS_NetworkSocket_close(handle);
--- system_config.h
+++ system_config.h
@@ -32,20 +32,18 @@
 // ChanMUX
 //-----------------------------------------------------------------------------
 
-#define CHANMUX_CHANNEL_NIC_1_CTRL      4
-#define CHANMUX_CHANNEL_NIC_1_DATA      5
+#define CHANMUX_CHANNEL_NIC_1_CTRL 4
+#define CHANMUX_CHANNEL_NIC_1_DATA 5
 
-#define CHANMUX_CHANNEL_NIC_2_CTRL      7
-#define CHANMUX_CHANNEL_NIC_2_DATA      8
-
+#define CHANMUX_CHANNEL_NIC_2_CTRL 7
+#define CHANMUX_CHANNEL_NIC_2_DATA 8
 
 //-----------------------------------------------------------------------------
 // ChanMUX clients
 //-----------------------------------------------------------------------------
 
-#define CHANMUX_ID_NIC_1        101
-#define CHANMUX_ID_NIC_2        102
-
+#define CHANMUX_ID_NIC_1 101
+#define CHANMUX_ID_NIC_2 102
 
 //-----------------------------------------------------------------------------
 // Network Stack
@@ -53,18 +51,17 @@
 
 #define CFG_TEST_HTTP_SERVER "192.168.82.12"
 
-//App1
+// App1
 #define DOMAIN_NWSTACK "STACK"
 
-#define CFG_ETH_ADDR_CLIENT "ETH_ADDR_CLIENT"
+#define CFG_ETH_ADDR_CLIENT       "ETH_ADDR_CLIENT"
 #define CFG_ETH_ADDR_CLIENT_VALUE "10.0.0.10"
 
-#define CFG_ETH_ADDR_SERVER "ETH_ADDR_SERVER"
+#define CFG_ETH_ADDR_SERVER       "ETH_ADDR_SERVER"
 #define CFG_ETH_ADDR_SERVER_VALUE "10.0.0.11"
 
-#define CFG_ETH_GATEWAY_ADDR "ETH_GATEWAY_ADDR"
+#define CFG_ETH_GATEWAY_ADDR       "ETH_GATEWAY_ADDR"
 #define CFG_ETH_GATEWAY_ADDR_VALUE "10.0.0.1"
 
-#define CFG_ETH_SUBNET_MASK "ETH_SUBNET_MASK"
+#define CFG_ETH_SUBNET_MASK       "ETH_SUBNET_MASK"
 #define CFG_ETH_SUBNET_MASK_VALUE "255.255.255.0"
-
