2693a3836adf5277e0e1b637dd38bef3ef42b21b
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-07-30 00:08:10 +0200

    [DEV] Lower ticker interval

--- components/Ticker/Ticker.c
+++ components/Ticker/Ticker.c
@@ -16,7 +16,7 @@ int run(void)
     Debug_LOG_INFO("ticker running");
 
     // set up a tick every second
-    int ret = timeServer_rpc_periodic(0, NS_IN_S);
+    int ret = timeServer_rpc_periodic(0, 1 * NS_IN_MS);
     if (0 != ret)
     {
         Debug_LOG_ERROR("timeServer_rpc_periodic() failed, code %d", ret);
