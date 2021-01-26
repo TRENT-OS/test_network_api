b2b1dde9b5126f4ea7b5d9dab9e370523c34f5be
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-01-26 15:04:27 +0100

    [DEV] SEOS-2011 Add rpi3 platform

---
+++ plat/rpi3/plat_nic.cmake
@@ -0,0 +1,11 @@
+#
+# Network API Test System, RPi QEMU configuration
+#
+# Copyright (C) 2020, Hensoldt Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+NIC_RPi_DeclareCAmkESComponent(
+    NIC_RPi
+)
