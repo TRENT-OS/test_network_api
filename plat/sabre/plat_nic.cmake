52c07ea2d134939cf0447313d6a8201f381034c3
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-03-05 14:25:48 +0100

    [DEV] SEOS-2431 add platform CMake file for sabre

---
+++ plat/sabre/plat_nic.cmake
@@ -0,0 +1,9 @@
+#
+# Network API Test System, i.MX6 Sabre Lite board configuration
+#
+# Copyright (C) 2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+set(LibEthdriverNumPreallocatedBuffers 32 CACHE STRING "" FORCE)
