ad78ccac7c3c511014d83a0beb653803a174b1cc
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-03-22 09:41:59 +0100

    [DEV] suppress cppcheck errors

--- util/loop.h
+++ util/loop.h
@@ -4,6 +4,8 @@
 
 #if __INCLUDE_LEVEL__ <= LOOP_COUNT
 LOOP_ELEMENT
+// TODO: check cppcheck warning: #include nested too deeply
+// cppcheck-suppress preprocessorErrorDirective
 #include "loop.h"
 #else
 #undef LOOP_COUNT
