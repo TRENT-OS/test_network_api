fc39efcc0772a5d15e3afeb30eaae4cf3a13abfb
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-07-25 16:57:24 +0200

    add second tcp server

--- test_configuration/tcp_server/test_configuration.cmake
+++ test_configuration/tcp_server/test_configuration.cmake
@@ -65,6 +65,24 @@ DeclareCAmkESComponent(
         syslogger_client
 )
 
+
+DeclareCAmkESComponent(
+    TestAppTCPServer_2
+    SOURCES
+        components/TestAppTCPServer/TestAppTCPServer.c
+    C_FLAGS
+        -Wall
+        -Werror
+    LIBS
+        system_config
+        os_core_api
+        lib_compiler
+        lib_debug
+        lib_macros
+        os_network_api
+        syslogger_client
+)
+
 DeclareCAmkESComponent_SysLogger(
     SysLogger
     system_config
