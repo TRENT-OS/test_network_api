aa2d39f29bbe589042dfe62c52688f9576b2571b
Lukas Graber <lukas.graber@hensoldt-cyber.de>
2021-11-04 16:15:22 +0100

    [DEV] SEOS-2345: Add RPi4 platform

---
+++ plat/rpi4/plat_nic.cmake
@@ -0,0 +1,11 @@
+#
+# Network API Test System, RPi4 configuration
+#
+# Copyright (C) 2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+NIC_RPi4_DeclareCAmkESComponent(
+    NIC_RPi4
+)
