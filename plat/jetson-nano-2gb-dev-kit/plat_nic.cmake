9086b1a8cc4f663661a214327e8e6dc288d4ea29
Florian Hauschild <florian.hauschild@hensoldt.net>
2022-08-11 14:48:12 +0200

    [DEV] SEOS-3394 WIP experimental getting test to run on jetson

---
+++ plat/jetson-nano-2gb-dev-kit/plat_nic.cmake
@@ -0,0 +1,19 @@
+#
+# Network API Test System, zynq7000 QEMU configuration
+#
+# Copyright (C) 2020-2021, HENSOLDT Cyber GmbH
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+ChanMux_UART_DeclareCAmkESComponents(
+    ChanMux_UART
+    components/ChanMux/ChanMux_config.c
+    system_config
+)
+
+NIC_ChanMux_DeclareCAmkESComponent(
+    NwDriver
+    CHANMUX_CHANNEL_NIC_CTRL
+    CHANMUX_CHANNEL_NIC_DATA
+)
